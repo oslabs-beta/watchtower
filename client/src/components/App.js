5fa12a2d79ea46eedb74a613dd45417cf750788c
Lauren <laurenfelty01@gmail.com>
2024-07-03 10:58:55 -0500

    add front-end boiler plate

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './Dashbaord.jsx';
+import NavBar from './NavBar.jsx';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
