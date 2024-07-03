daf8be11986103a0077bdb9866e28dc62e748199
Lauren <laurenfelty01@gmail.com>
2024-07-03 16:00:58 -0500

    mock add data to graphs

--- client/src/components/TotalTimeGraphContainer.jsx
+++ client/src/components/TotalTimeGraphContainer.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import '../styles/graphContainer.scss';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  { rcu: 40, time: '1' },
+  { rcu: 100, time: '2' },
+  { rcu: 23, time: '3' },
+  { rcu: 33, time: '4' },
+  { rcu: 54, time: '5' },
+  { rcu: 88, time: '6' },
+  { rcu: 26, time: '7' },
+  { rcu: 77, time: '8' },
+  { rcu: 33, time: '9' },
+  { rcu: 86, time: '10' },
+];
 
 function TotalTimeGraphContainer() {
-  return <div className = 'indvidualGraph'>Total Time Graph</div>;
+  return (
+    <div className='indvidualGraph'>
+     Total Time
+      <ResponsiveContainer width='100%' height={400}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='time' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' dataKey='rcu' stroke='#000000' />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default TotalTimeGraphContainer;
--- client/src/components/WcuGraphContainer.jsx
+++ client/src/components/WcuGraphContainer.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import '../styles/graphContainer.scss';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  { wcu: 40, time: '1' },
+  { wcu: 100, time: '2' },
+  { wcu: 23, time: '3' },
+  { wcu: 33, time: '4' },
+  { wcu: 54, time: '5' },
+  { wcu: 88, time: '6' },
+  { wcu: 26, time: '7' },
+  { wcu: 77, time: '8' },
+  { wcu: 33, time: '9' },
+  { wcu: 86, time: '10' },
+];
 
 function WcuGraphContainer() {
-  return <div className='indvidualGraph'>WCU Graph Container</div>;
+  return (
+    <div className='indvidualGraph'>
+      WCU Graph
+      <ResponsiveContainer width='100%' height={400}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='time' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' dataKey='wcu' stroke='#000000' />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default WcuGraphContainer;
