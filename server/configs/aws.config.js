1bb76ffc6a6201b2b108afb347499c80be489435
James Coen <jcoen96@gmail.com>
2024-07-09 18:08:40 -0600

    set up metricController.ts to include middleware for accessing table metrics from AWS Cloudwatch

--- server/configs/aws.config.js
+++ server/configs/aws.config.js
@@ -1,23 +1,63 @@
-require('dotenv/config.js');
-// const AWS = require('aws-sdk');
-// const { fromEnv } = require('@aws-sdk/credential-providers');
-// const {
-//   DynamoDBClient,
-//   DescribeTableCommand,
-// } = require('@aws-sdk/client-dynamodb');
-const { CloudWatchClient } = require('@aws-sdk/client-cloudwatch');
+// require('dotenv/config.js');
+// const { CloudWatchClient } = require('@aws-sdk/client-cloudwatch');
+// const { GetMetricStatisticsCommand } = require('@aws-sdk/client-cloudwatch'); //added just for testing purposes
+
+import 'dotenv/config.js';
+import {
+  CloudWatchClient,
+  GetMetricStatisticsCommand,
+} from '@aws-sdk/client-cloudwatch';
 
 const cloudWatchClient = new CloudWatchClient({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
   },
-  region: process.env.region,
+  region: process.env.REGION,
 });
+// console.log(process.env.AWS_ACCESS_KEY_ID);
 
-module.exports = cloudWatchClient;
+// console.log(cloudWatchClient);
 
+// module.exports = cloudWatchClient;
 
+const cloudWatchInput = {
+  // GetMetricStatisticsInput
+  Namespace: 'AWS/DynamoDB', // required
+  MetricName: 'ProvisionedReadCapacityUnits', // required
+  Dimensions: [
+    // Dimensions
+    {
+      // Dimension
+      Name: 'TableName', // required
+      Value: 'mockUserTable1', // required
+    },
+  ],
+  StartTime: new Date('2024-07-08T15:20:00.000Z'), // required
+  EndTime: new Date('2024-07-08T15:30:00.000Z'), // required
+  Period: Number('60'), // required
+  Statistics: [
+    // Statistics
+    'Average',
+    // 'Maximum',
+    // 'SampleCount' || 'Average' || 'Sum' || 'Minimum' || 'Maximum',
+  ],
+  //   ExtendedStatistics: [
+  //     // ExtendedStatistics
+  //     'STRING_VALUE',
+  //   ],
+  //   Unit: 'Count/Second',
+};
+
+const getMetricStatisticsCommand = new GetMetricStatisticsCommand(
+  cloudWatchInput
+);
+// console.log(getMetricStatisticsCommand);
+
+const cloudWatchResponse = await cloudWatchClient.send(
+  getMetricStatisticsCommand
+);
+console.log(cloudWatchResponse);
 
 // //sdk v3 configuration method for DynamoDB:
 
