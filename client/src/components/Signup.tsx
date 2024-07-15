d76c70807aaddabc4e96f98fc20a324dd512a270
Lauren <laurenfelty01@gmail.com>
2024-07-15 11:19:27 -0500

    remove check box from sign up page

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -112,12 +112,12 @@ export default function Signup() {
                 />
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel
+                {/* <FormControlLabel
                   control={
                     <Checkbox value='allowExtraEmails' color='primary' />
                   }
                   label='I want to receive inspiration, marketing promotions and updates via email.'
-                />
+                /> */}
               </Grid>
             </Grid>
             <Button
