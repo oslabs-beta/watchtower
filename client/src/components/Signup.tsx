947ce565a0e176b811c2c42c881a2273993bc1d3
Lauren <laurenfelty01@gmail.com>
2024-07-16 07:53:27 -0500

    merge with update ui

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -5,7 +5,6 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
