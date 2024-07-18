fbbcb67af8e60817a6f05fcfa3beaefd46debd8c
Piero Espejo <Piero914@gmail.com>
2024-07-18 16:04:57 -0400

    Testing config 2.0

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,23 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export {};
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    // if your using tsconfig.paths thers is no harm in telling jest
+    '@components/(.*)$': '<rootDir>/src/components/$1',
+    '@/(.*)$': '<rootDir>/src/$1',
+
+    // mocking assests and styling
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/mocks/fileMock.ts',
+    '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mocks/styleMock.ts',
+    /* mock models and services folder */
+    '(assets|models|services)': '<rootDir>/tests/mocks/fileMock.ts',
   },
+  // to obtain access to the matchers.
+  setupFilesAfterEnv: ['./tests/setupTests.ts'],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
 };
