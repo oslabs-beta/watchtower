fdbe7fe1f46e6b06198db025608f89ceb0c17302
Piero Espejo <Piero914@gmail.com>
2024-07-19 17:01:33 -0400

    Modified Jest Configuration and Created first Test Environment

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,23 +1,14 @@
-export {};
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  moduleNameMapper: {
-    // if your using tsconfig.paths thers is no harm in telling jest
-    '@components/(.*)$': '<rootDir>/src/components/$1',
-    '@/(.*)$': '<rootDir>/src/$1',
-
-    // mocking assests and styling
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/mocks/fileMock.ts',
-    '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mocks/styleMock.ts',
-    /* mock models and services folder */
-    '(assets|models|services)': '<rootDir>/tests/mocks/fileMock.ts',
-  },
-  // to obtain access to the matchers.
-  setupFilesAfterEnv: ['./tests/setupTests.ts'],
-
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePaths: ['<rootDir>'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next', '<rootDir>/node_modules/'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
 };
---
+++ test/utils/index.test.ts
@@ -0,0 +1,8 @@
+// Now that it seems that configuration is FINALLY set, we can start writing functional Tests
+// The plan is two perform some testing for the following:
+// 1. Unit Testing
+// 2. React Component Testing
+// 3. Endpoint Testing (Express / MongoDB)
+// 4. AI Testing? (Research if this is viable or industry standard at all)
+
+import {} from '../../client/dist/index';
