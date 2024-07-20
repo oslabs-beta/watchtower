e16140c7038a213e1c6d32ef06d00ed488a0f34f
Piero Espejo <Piero914@gmail.com>
2024-07-20 11:01:17 -0400

    testing configuration with @types/jest

--- test/utils/index.test.ts
+++ test/utils/index.test.ts
@@ -1,8 +1,23 @@
-// Now that it seems that configuration is FINALLY set, we can start writing functional Tests
-// The plan is two perform some testing for the following:
+import 'jest';
+import '@types/jest';
+// The plan is to perform some testing for the following:
 // 1. Unit Testing
 // 2. React Component Testing
 // 3. Endpoint Testing (Express / MongoDB)
 // 4. AI Testing? (Research if this is viable or industry standard at all)
 
-import {} from '../../client/dist/index';
+// Create a sample test to ensure Jest Configuration is valid
+
+// test('testing that jest is functioning', () => {
+//   expect(2 + 2).toEqual(4);
+// });
+
+describe('Very first test', () => {
+  it('A test', () => {
+    const hello = () => {
+      'helloWorld';
+    };
+    // Testing that hello function actually returns string helloWorld
+    expect(hello()).toBe('helloWorld');
+  });
+});
